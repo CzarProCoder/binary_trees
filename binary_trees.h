5b688235b9a48bc4e503bda1a43905fdbe6c7046
CzarProCoder <juliusczar36@gmail.com>
2023-06-01 22:02:08 +0300

    Function that checks if a binary tree is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 
 int is_full_recursive(const binary_tree_t *tree);
 
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
